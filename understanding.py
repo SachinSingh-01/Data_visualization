3c4b7ee838c57b35e54c5d76ce12a5388ed5a9bf
Sachin Singh <sachinsingh19012@gmail.com>
2026-05-04 12:19:47 +0530

    start matplotlib

--- understanding.py
+++ understanding.py
@@ -2,28 +2,48 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 x=np.linspace(0,5,11)
-x
-y=(x==3)
+y=x**2
+
+plt.figure()
 plt.plot(x,y)
-y
-plt.title("Hello")
+plt.title("Basic plot")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
+# plt.show()
+
+plt.figure()
 plt.subplot(2,2,1)
+plt.plot(x,y)
+plt.title("Plot 1")
+
 plt.subplot(2,2,2)
+plt.plot(x,y*2)
+plt.title("Plot 2")
+
 plt.subplot(2,2,3)
+plt.plot(x,y*3)
+plt.title("Plot 3")
+
 plt.show()
 
-# fig=plt.figure()
-# a=fig.add_axes([0.1,0.1,1,1])
-# result=a.plot(x,y)
-# print(result)
+fig=plt.figure()
+a=fig.add_axes([0.1,0.1,1,1])
+a.plot(x,y)
+a.set_title("Main plot")
 
-# plt.subplot(2,2,1)
-# plt.subplot(2,2,3)
-# plt.subplot(2,2,2)
-# plt.subplot(2,2,4)
-# plt.plot(x,y)
-# fig=plt.figure()
-# b-fig.add_axes([0,3,0.4,0.3,0.3])
-# b.plot(x,y)
+b=fig.add_axes([0.3,0.4,0.3,0.3])
+b.plot(x,y)
+b.set_title("Small plot")
+
+plt.show()
+
+fig=plt.figure(figsize=(20,8),dpi=100)
+axis1=fig.add_axes([0.1,0.1,.4,.8])
+axis1.plot(x,y)
+axis1.set_title("dpi")
+
+axis2=fig.add_axes([0.3,0.4,.3,.3])
+axis2.plot(x,y)
+axis2.set_title("big_dpi")
+
+plt.show()
