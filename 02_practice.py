9b1abf38def8faf8f2198250cf8ec3beb85d7030
Sachin Singh <sachinsingh19012@gmail.com>
2026-05-16 21:01:25 +0530

    done this topic

--- 02_practice.py
+++ 02_practice.py
@@ -812,21 +812,27 @@ debug issue
 Goal
 Experience pyplot confusion practically.'''
 # plt.figure(figsize=(8,2))
-# plt.subplot(1,2,1)
+# plt.subplot(2,2,1)
 # plt.plot([56,76,45],[3,4,5])
 # plt.title("First figure")
-# plt.figure(figsize=(8,2))
-# plt.subplot(1,2,1)
-# plt.plot([56,76,45],[3,4,5])
-# plt.title("First figure")
-# plt.figure(figsize=(8,2))
-# plt.subplot(1,2,1)
-# plt.plot([56,76,45],[3,4,5])
-# plt.title("First figure")
-# plt.figure(figsize=(8,2))
-# plt.subplot(1,2,1)
-# plt.plot([56,76,45],[3,4,5])
-# plt.title("First figure")
+
+# plt.subplot(2,2,2)
+# plt.plot([76,34,23],[6,9,2])
+# plt.title("Second figure")
+
+# plt.subplot(2,2,3)
+# plt.plot([5,7,5],[67,34,21])
+# plt.title("Third figure")
+
+# plt.subplot(2,2,4)
+# plt.plot([76,45,45],[23,45,12])
+# plt.title("Fourth figure")
+
+# # Modify
+# plt.subplot(2,2,1)
+# plt.title("Modified")
+
+# plt.show()
 
 # Question 31
 '''Create dashboard:
@@ -901,29 +907,101 @@ save figure
 ONLY explicit interface allowed.
 Goal
 Simulate real project architecture.'''
-def my_plot(ax,x,y):
-    ax.plot(x,y,label="data")
-x=[6,4,3,2]
-y=[5,9,2,1]
-fig,ax=plt.subplots(2,2,figsize=(8,2))
-my_plot(ax[0,0],x,y)
-ax[0,0].set_title("First figure")
+# def my_plot(ax,x,y):
+#     ax.plot(x,y,label="data")
+# x=[6,4,3,2]
+# y=[5,9,2,1]
+# fig,ax=plt.subplots(2,2,figsize=(8,2))
+# my_plot(ax[0,0],x,y)
+# ax[0,0].set_title("First figure")
+# ax[0,0].legend(loc="upper left")
+# ax[0,0].annotate("Highest",(5,2))
+
+# my_plot(ax[0,1],x,y)
+# ax[0,1].set_title("Second figure")
+# ax[0,1].legend(loc="upper right")
+# ax[0,1].annotate("Highest",(4,3))
+
+# my_plot(ax[1,0],x,y)
+# ax[1,0].set_title("Third figure")
+# ax[1,0].legend(loc="lower left")
+# ax[1,0].annotate("Highest",(2,5))
+
+# my_plot(ax[1,1],x,y)
+# ax[1,1].set_title("Fourth figure")
+# ax[1,1].legend(loc="lower right")
+# ax[1,1].annotate("Highest",(3,3))
+# plt.savefig("Mini_dataplot.png")
+# plt.show()
+
+# Question 35
+'''Try creating:
+6 subplot dashboard
+First with pyplot only.
+Then with explicit axes.
+Compare:
+debugging
+readability
+control
+Goal
+Feel WHY explicit interface wins.'''
+plt.figure(figsize=(8,2))
+
+plt.subplot(2,3,1)
+plt.plot([56,76,45],[3,4,5])
+plt.title("First figure")
+
+plt.subplot(2,3,2)
+plt.plot([76,34,23],[6,9,2])
+plt.title("Second figure")
+
+plt.subplot(2,3,3)
+plt.plot([5,7,5],[67,34,21])
+plt.title("Third figure")
+
+plt.subplot(2,3,4)
+plt.plot([76,45,45],[23,45,12])
+plt.title("Fourth figure")
+
+plt.subplot(2,3,5)
+plt.plot([45,23,62],[23,4,1])
+plt.title("Fifth figure")
+
+plt.subplot(2,3,6)
+plt.plot([54,2,12],[23,61,23])
+plt.title("Sixth figure")
+plt.show()
+
+# Explicit function
+
+fig,ax=plt.subplots(2,3)
+ax[0,0].plot([4,5,7],[6,4,3],label="Data 1")
 ax[0,0].legend(loc="upper left")
-ax[0,0].annotate("Highest",(5,2))
+ax[0,0].annotate("Highest",(4,6))
+ax[0,0].set_title("Fig-1")
 
-my_plot(ax[0,1],x,y)
-ax[0,1].set_title("Second figure")
-ax[0,1].legend(loc="upper right")
-ax[0,1].annotate("Highest",(4,3))
+ax[0,1].plot([7,5,3],[4,3,2],label="Data 2")
+ax[0,1].legend(loc="upper left")
+ax[0,1].annotate("Highest",(5,2))
+ax[0,1].set_title("Fig-2")
 
-my_plot(ax[1,0],x,y)
-ax[1,0].set_title("Third figure")
-ax[1,0].legend(loc="lower left")
-ax[1,0].annotate("Highest",(2,5))
+ax[1,0].plot([6,7,4],[9,2,4],label="Data 3")
+ax[1,0].legend(loc="lower right")
+ax[1,0].annotate("Highest",(4,4))
+ax[1,0].set_title("Fig-3")
 
-my_plot(ax[1,1],x,y)
-ax[1,1].set_title("Fourth figure")
+ax[1,1].plot([3,2,6],[7,8,3],label="Data 4")
 ax[1,1].legend(loc="lower right")
-ax[1,1].annotate("Highest",(3,3))
-plt.savefig("Mini_dataplot.png")
+ax[1,1].annotate("Highest",(7,4))
+ax[1,1].set_title("Fig 4")
+
+ax[0,2].plot([7,2,8],[2,9,4],label="Data 5")
+ax[0,2].legend(loc="lower right")
+ax[0,2].annotate("Highest",(4,3))
+ax[0,2].set_title("Fig 5")
+
+ax[1,2].plot([5,8,3],[9,4,2],label="Data 6")
+ax[1,2].legend(loc="lower right")
+ax[1,2].annotate("Highest",(2,5))
+ax[1,2].set_title("Fig 6")
 plt.show()
